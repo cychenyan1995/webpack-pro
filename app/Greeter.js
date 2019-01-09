6adc0f2155107b9377e7a057adc9a869c54bc5b7
cy <1593225054@qq.com>
2019-01-09 16:37:22 +0800

    webpack

---
+++ app/Greeter.js
@@ -0,0 +1,23 @@
+// 使用ES6的方法
+import React, {Component} from 'react'
+import config from './config.json';
+import style from './Greeter.css'
+
+class Greeter extends Component{
+  render(){
+    return (
+      <div className={style.root}>
+        {config.greetText}
+      </div>
+      )
+  }
+}
+
+export default Greeter
+// var config = require('./config.json')
+// module.exports = function() {
+//   var greet = document.createElement('div');
+//   greet.textContent = config.greetText;
+//   return greet;
+// };
+
